be4c6d3d9fab216fb3d580a32df0a6a6c44e30a7
vinod.rawat <Engineerlp@4164>
2020-01-13 11:16:08 +0530

    some

--- redis-spring/src/main/java/com/redis/test/redisspring/dao/ProgrammerDaoImpl.java
+++ redis-spring/src/main/java/com/redis/test/redisspring/dao/ProgrammerDaoImpl.java
@@ -1,20 +1,25 @@
 package com.redis.test.redisspring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
 @Repository
 public class ProgrammerDaoImpl implements ProgrammerRepository {
 
-//    @Autowired
-//    RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
     @Override
     public void setProgrammerAsString(String idKey, String programmer) {
-//        redisTemplate.opsForValue().set(idKey,programmer);
-//        redisTemplate.expire(idKey,100, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set(idKey,programmer);
+        redisTemplate.expire(idKey,100, TimeUnit.SECONDS);
     }
 
     @Override
     public String getProgrammer(String idKey) {
-//        return (String) redisTemplate.opsForValue().get(idKey);
-        return null;
+        return (String) redisTemplate.opsForValue().get(idKey);
+//        return null;
     }
 }
